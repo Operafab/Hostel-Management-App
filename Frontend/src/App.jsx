9ce5244322987962b498e7fafb98b6bd2cd905f3
raphael faboyinde <raphaelfaboyinde27@gmail.com>
2024-11-04 12:50:56 +0100

    next commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,24 @@
 
-// import { Route, Routes } from 'react-router-dom'
-// import './App.css'
-// import AdminReg from "./com"
-
-import AdminReg from "./Component/Register/AdminReg"
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import AdminReg from './Component/Register/adminReg'
+import Header from "./Component/Header/Header"
 import StudentReg from "./Component/Register/StudentReg"
+// import AdminReg from "./Component/Register/AdminReg"
 
 
 function App() {
-   
+   const renderRoute = ()=>(
+    <Routes>
+      <Route path="/" element={<AdminReg/>}/>
+    </Routes>
+
+   )
+
+
   return (
     <>
-   {/* <AdminReg/> */}
-
-   <StudentReg/>
+      {renderRoute()}
     </>
   )
 }
--- Frontend/src/Component/Register/Login.jsx
+++ Frontend/src/Component/Register/Login.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  })
+  const [formValidMessage, setFormValidMessage] = useState("")
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const navigate = useNavigate();
+
+  const handleInputChange = () => {
+
+  }
+
+
+  const loginUser = () => {
+
+  }
+
   return (
-    <div>
-      
+    <div className='container form__ --100vh'>
+      <div className="form-container">
+        <p className="title">Admin Login</p>
+        <form className="form" onSubmit={loginUser}>
+          <div className="--dir-column">
+            <label htmlFor="email">Email:</label>
+            <input type="email" className="input" name='email' placeholder='Enter your email' required value={formData.email} onChange={handleInputChange} />
+          </div>
+
+          <div className="--dir-column">
+            <label htmlFor="password">Password: </label>
+            <input
+              placeholder='Password'
+              name='password'
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button className='--btn' disabled={isSubmitting}>
+            {isSubmitting ? "Signing in...": "Sign In"}
+          </button>
+        </form>
+        {formValidMessage && (
+          <p className="error-message">
+            {formValidMessage}
+          </p>
+        )}
+        <p>
+          Don't have an account yet ? <Link to="/">Sign Up</Link>
+        </p>
+      </div>
     </div>
   )
 }
