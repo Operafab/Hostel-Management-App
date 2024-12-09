7e3307530ecb7e627a9c2af29bc103bc77c82417
raphael faboyinde <raphaelfaboyinde27@gmail.com>
2024-12-09 16:00:10 +0100

    linking

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,45 +1,52 @@
-import { Route, Routes } from 'react-router-dom'
-import Header from './Component/Header/Header'
-import AdminReg from './Component/Register/AdminReg'
-import StudentReg from './Component/Register/StudentReg'
-import  Login  from './Component/Register/Login'
-import Layout from './Component/Layout/Layout'
-import AdminPreview from './Component/AdminPreview/AdminPreview'
-import HomeDash from './Component/Dashboard/HomeDash'
-import StudentDashboard from './Component/Dashboard/StudentDashBoard'
-import Room from './Component/Dashboard/Room'
-import RequireAuth from './context/RequireAuth'
- 
+import { Route, Routes } from "react-router-dom";
+import Header from "./Component/Header/Header";
+import AdminReg from "./Component/Register/AdminReg";
+import StudentReg from "./Component/Register/StudentReg";
+import Login from "./Component/Register/Login";
+import Layout from "./Component/Layout/Layout";
+import AdminPreview from "./Component/AdminPreview/AdminPreview";
+import HomeDash from "./Component/Dashboard/HomeDash";
+import StudentDashboard from "./Component/Dashboard/StudentDashBoard";
+import Room from "./Component/Dashboard/Room";
+import RequireAuth from "./context/RequireAuth";
 
 function App() {
   const RenderRoute = () => (
     <Routes>
-        <Route path='/' element={<AdminReg/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route element={<RequireAuth/>}>
-        <Route path='/studentreg' element={
-          <Layout>
-            <StudentReg/>
-          </Layout>
-        }/>
-        <Route path='/adminprev' element={<AdminPreview/>}/>
-        <Route path='/home-dash' element={
-          <Layout>
-            <HomeDash/>
-          </Layout>
-        }/>
-        <Route path='/student-dash' element={<StudentDashboard/>}/>
-        <Route path='/room' element={<Room/>}/>
-        </Route>
-        
+      <Route path="/" element={<AdminReg />} />
+      <Route path="/login" element={<Login />} />
+      <Route element={<RequireAuth />}>
+        <Route
+          path="/studentreg"
+          element={
+            <Layout>
+              <StudentReg />
+            </Layout>
+          }
+        />
+        <Route
+          path="/adminprev"
+          element={
+            <Layout>
+              <AdminPreview />
+            </Layout>
+          }
+        />
+        <Route
+          path="/home-dash"
+          element={
+            <Layout>
+              <HomeDash />
+            </Layout>
+          }
+        />
+        <Route path="/student-dash" element={<StudentDashboard />} />
+        <Route path="/room" element={<Room />} />
+      </Route>
     </Routes>
-  )
+  );
 
-  return (
-    <>
-    {RenderRoute()}
-    </>
-  )
+  return <>{RenderRoute()}</>;
 }
 
-export default App
+export default App;
--- Frontend/src/Component/Dashboard/HomeDash.jsx
+++ Frontend/src/Component/Dashboard/HomeDash.jsx
@@ -1,108 +1,164 @@
+import React, { useContext, useEffect, useState } from "react";
+import "./HomeDash.css";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../context/userContext";
+import axios from "axios";
+import { ClipLoader } from "react-spinners";
+import { toast } from "react-toastify";
+import { formatDistanceToNow } from "date-fns";
 
-import React, { useContext, useEffect, useState } from 'react';
-import './HomeDash.css';
-import { Link } from 'react-router-dom';
-import { UserContext } from '../../context/userContext';
-import axios from 'axios';
-import { ClipLoader } from 'react-spinners';
-import { toast } from 'react-toastify';
 
-// const activities = [
-//     {img: '/src/assets/asset-1.png', name: 'Jenny', action: 'Jenny just checked out', time:'3 mins ago'},
-//     {img: '/src/assets/lady.png', name: 'John', action: 'John checked in', time:'3 mins ago'},
-//     {img: '/src/assets/asset-1.png', name: 'Peter', action: 'Peter checked out', time:'3 mins ago'}
-// ]
 const override = {
-    display: "block",
-    margin: "100px auto",
-  };
+  display: "block",
+  margin: "100px auto",
+};
+
+const formatName = (fullname) => {
+  const firstName = fullname.split(" ")[0];
+  return firstName;
+};
 const HomeDash = () => {
-    const {user} = useContext(UserContext);
-    const [isLoading, setIsLoading] = useState(true);
-    const [data, setData] = useState([]);
-    const [checkedInCount, setCheckedInCount] = useState(0);
-    const [checkedOutCount, setCheckedOutCount] = useState(0);
-    const [checkedInStudents, setCheckedInStudents] = useState([]);
-    const [checkedOutStudents, setCheckedOutStudents] = useState([]);
-    useEffect(()=>{
-        const fetchStudents = async() =>{
-            try {
-               const response = await axios.get("http://localhost:5000/student", {withCredentials:true});
-               const data = response.data 
-               console.log(data)
-               setData(data);
-               const checkedInStds = data.filter((student)=>student.checkedIn);
-               setCheckedInCount(checkedInStds.length)
-               setCheckedOutStudents(checkedInStds); 
+  const { user } = useContext(UserContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const [students, setStudents] = useState([]);
+  const [checkedInCount, setCheckedInCount] = useState(0);
+  const [checkedOutCount, setCheckedOutCount] = useState(0);
+  const [checkedInStudents, setCheckedInStudents] = useState([]);
+  const [checkedOutStudents, setCheckedOutStudents] = useState([]);
+  useEffect(() => {
+    const fetchStudents = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/student", {
+          withCredentials: true,
+        });
+        const data = response.data;
+        console.log(data);
+        setStudents(data);
+        const checkedInStds = data.filter((student) => student.checkedIn);
+        setCheckedInCount(checkedInStds.length);
+        setCheckedOutStudents(checkedInStds);
 
-               const checkedOutStds = data.filter((student)=>!student.checkedIn);
-               setCheckedOutCount(checkedOutStds.length);
-               setCheckedOutStudents(checkedOutStds);
-            } catch (error) {
-                console.log(error)
-                toast.error(error?.response?.data?.message);
-            }finally{
-                setIsLoading(false);
-            }
-        };
-        fetchStudents();
-    },[]);
-
-    if (isLoading) {
-        <ClipLoader color="#3a86ff" cssOverride={override} loading={isLoading} />;
+        const checkedOutStds = data.filter((student) => !student.checkedIn);
+        setCheckedOutCount(checkedOutStds.length);
+        setCheckedOutStudents(checkedOutStds);
+      } catch (error) {
+        console.log(error);
+        toast.error(error?.response?.data?.message);
+      } finally {
+        setIsLoading(false);
       }
+    };
+    fetchStudents();
+  }, []);
+
+  if (isLoading) {
+    <ClipLoader color="#3a86ff" cssOverride={override} loading={isLoading} />;
+  }
   return (
-    <div className='--flex-center __homeDashCon'>
-        <div className="__paraCon">
-            <h1 className='__paraHeader'>Welcome Back, Jackie!</h1>
+    <div className="--flex-center __homeDashCon">
+      <div className="__paraCon">
+        <h1 className="__paraHeader">
+          Welcome Back, {formatName(user.fullname)}!
+        </h1>
+      </div>
+      <div className="__secondCon">
+        <h3 className="__quickTitle">Quick Stats</h3>
+        <div className="__flex __boards">
+          <div className="__board">
+            <p className="__boardHead">{students.length}</p>
+            <p className="__boardDetails">Total Students</p>
+          </div>
+          <div className="__board">
+            <p className="__boardHead">{checkedInCount}</p>
+            <p className="__boardDetails">Active Students</p>
+          </div>
+          <div className="__board">
+            <p className="__boardHead">{checkedOutCount}</p>
+            <p className="__boardDetails">Inactive Students</p>
+          </div>
         </div>
-        <div className="__secondCon">
-            <h3 className="__quickTitle">Quick Stats</h3>
-            <div className="__flex __boards">
-                <div className="__board">
-                    <p className="__boardHead">120</p>
-                    <p className="__boardDetails">Total Students</p>
-                </div>
-                <div className="__board">
-                    <p className="__boardHead">100</p>
-                    <p className="__boardDetails">Active Students</p>
-                </div>
-                <div className="__board">
-                    <p className="__boardHead">20</p>
-                    <p className="__boardDetails">Inactive Students</p>
-                </div>
-            </div>
-        </div>
+      </div>
+      
+
         <div className="--flex-center __firstCon">
-            <h4 className="__title">Recent Activities</h4>
-                {activities.map((activity) => (
-                    <div className="__users">
-                        <div className="__firstUserPic">
-                            <img src={activity.img} alt={activity.name} />
-                        </div>
-                        <div className="__userData">
-                            <div>
-                                <h5>{activity.name}</h5>
-                                <p>{activity.action}</p>
-                            </div>
-                            <p>{activity.time}</p>
-                        </div>
-                    </div>
-                ))}
+          <h4 className="__title">Recent Activities.</h4>
+
+          <div className="__users">
+            <table className="home_table">
+              <tbody>
+                {checkedInStudents.length > 0 ? (
+                  checkedInStudents.map((student) => {
+                    const { name, checkedInTime, _id } = student;
+                    return (
+                      <tr className="table_data" key={_id}>
+                        <td className="table_data">
+                          {name}
+                          <p>{formatName(name)} has checked in</p>
+                        </td>
+                        <td>
+                          {formatDistanceToNow(checkedInTime, {
+                            addSuffix: true,
+                          })}
+                        </td>
+                      </tr>
+                    );
+                  })
+                ) : (
+                  <tr>
+                    <td colSpan={2}>No Recent CheckIns</td>
+                  </tr>
+                )}
+              </tbody>
+              
+            </table>
+          </div>
+          <div className="__users">
+            <table className="home_table">
+              <tbody>
+                {checkedOutStudents.length > 0 ? (
+                  checkedOutStudents.map((student) => {
+                    const { name, checkedOutTime, _id } = student;
+                    return (
+                      <tr className="table_data" key={_id}>
+                        <td className="table_data">
+                          {name}
+                          <p>{formatName(name)} has checked in</p>
+                        </td>
+                        <td>
+                          {formatDistanceToNow(checkedOutTime, {
+                            addSuffix: true,
+                          })}
+                        </td>
+                      </tr>
+                    );
+                  })
+                ) : (
+                  <tr>
+                    <td colSpan={2}>No Recent CheckIns</td>
+                  </tr>
+                )}
+              </tbody>
+
+            </table>
+          </div>
         </div>
-        <div className="__lastCon">
-            <h3 className="__lastTitle">Quick Actions</h3>
-            <div>
-                <button className="__addBtn">
-                    <Link style={{color: 'white'}} to='/studentreg'>Add student</Link>
-                </button>
-                <button className="__attendBtn">
-                    <Link to='#'>Attendance</Link>
-                </button>
-            </div>
+
+       
+      <div className="__lastCon">
+        <h3 className="__lastTitle">Quick Actions</h3>
+        <div>
+          <button className="__addBtn">
+            <Link style={{ color: "white" }} to="/studentreg">
+              Add student
+            </Link>
+          </button>
+          <button className="__attendBtn">
+            <Link to="#">Attendance</Link>
+          </button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default HomeDash;
